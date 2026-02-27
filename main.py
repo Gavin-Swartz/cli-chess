66fd01c3dbc0e869a51cdc45807f656881ba1cd2
Gavin <gvndswartz@gmail.com>
2026-02-26 19:55:06 -0600

    Added more complete input error handling and logic to allow user to try again if input is faulty

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ from setup import instantiate_pieces, instantiate_players
 # TODO: move or update logic for converting chars to ints
 col_letters = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
-def process_command(command, player: Player):
+def process_command(command, player: Player) -> bool:
     # Command structure:
     # Ex: E2 E4
     # (from) (to)
-    # TODO: check valid command structure
 
-    # Parse command
-    start, dst = command.split(' ')
+    # Parse command and check command validity
+    parsed_command = command.split(' ')
+
+    # Command should have exactly 2 arguments
+    if len(parsed_command) != 2:
+        print('Invalid command entered. Try again.')
+        return False
+    for arg in parsed_command:
+        # Each argument in command should have 2 characters: the first alphabetical and the second a numeric digit
+        if (len(arg) != 2) or not (arg[0].isalpha()) or not (arg[1].isdigit()):
+            print('Invalid command entered. Try again.')
+            return False
+    start, dst = parsed_command
 
     # Get starting index
-    start_col = col_letters[start[0]]
-    start_row = 8 - int(start[1])
+    start_col = col_letters[start[0].upper()]   # Get letter (column)
+    start_row = 8 - int(start[1])               # Get number (row)
 
     # Get piece at location
     selected_piece = Piece(-1, -1, True)    # Will always be invalid, used as placeholder
@@ -27,17 +37,17 @@ def process_command(command, player: Player):
             print(f'Piece found: {piece.rep}')
 
     if selected_piece.captured:
-        # TODO: allow player to try again
-        print('Invalid piece selected! Try again!')
+        print('Invalid piece selected. Try again.')
+        return False
     else:
-
-        # Get destination index
         # TODO: check valid (on board, within piece limitations, no existing piece from same player)
+        # Get destination index
         dst_col = col_letters[dst[0]]
         dst_row = 8 - int(dst[1])
 
         # Move piece
         selected_piece.move(dst_col, dst_row)
+        return True
 
 
 def main():
@@ -56,8 +66,10 @@ def main():
             display_board(board)
 
             # Get user input
-            command = input(f"({player.color}) Enter Move: ")
-            process_command(command, player)
+            valid_command = False
+            while not valid_command:
+                command = input(f"({player.color}) Enter Move: ")
+                valid_command = process_command(command, player)
             
             # Update board
             board = generate_board(white, black)
