612f5c483d511e2e33c090b27ffbc5725932c9c7
Gavin <gvndswartz@gmail.com>
2026-03-23 18:24:53 -0500

    Valid queen movement

--- pieces/queen.py
+++ pieces/queen.py
@@ -1,9 +1,28 @@
 from pieces.piece import Piece
+from pieces.piece_utils import validate_rook_bishop_squares
 
 
 class Queen(Piece):
     rep = 'Q'
     name = 'queen'
+    available_directions = [[1, 1], [-1, 1], [1, -1], [-1, -1], [0, 1], [0, -1], [1, 0], [-1, 0]]
 
     def update_valid_moves(self, player, opponent):
-        return super().update_valid_moves(player, opponent)
+        # Reset possible moves
+        self.possible_moves = []
+
+        # Captured pieces have no possible moves
+        if self.captured:
+            return
+        
+        # Search in all 4 diagonals and all 4 directions
+        for direction in self.available_directions:
+            newX = self.x
+            newY = self.y
+
+            searching = True
+            while searching:
+                newX += direction[0]
+                newY += direction[1]
+
+                searching = validate_rook_bishop_squares(player, opponent, newX, newY, self)
