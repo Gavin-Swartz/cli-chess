391160fcb304d6288e013461d63354df70c76ce2
Gavin <gvndswartz@gmail.com>
2026-04-21 21:48:50 -0500

    King can move one square in any direction, other king rules not implemented

--- pieces/king.py
+++ pieces/king.py
@@ -1,4 +1,5 @@
 from pieces.piece import Piece
+from board import get_piece_at_square, on_board
 
 
 class King(Piece):
@@ -6,4 +7,19 @@ class King(Piece):
     name = 'king'
 
     def update_valid_moves(self, player, opponent):
-        return super().update_valid_moves(player, opponent)
+        # Reset possible moves
+        self.possible_moves = []
+
+        # Captured pieces have no possible moves
+        if self.captured:
+            return
+        
+        # Check for valid moves in all 8 potential squares
+        for dY in [-1, 0, 1]:
+            for dX in [-1, 0, 1]:
+                newX = self.x + dX
+                newY = self.y + dY
+
+                if on_board(newX, newY):
+                    if get_piece_at_square(player.pieces, newX, newY).captured:
+                        self.possible_moves.append([newX, newY])
